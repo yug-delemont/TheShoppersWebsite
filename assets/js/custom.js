be095b77324609615c1b198747ea2115ca705b2f
Yug Jadvani <yugjadvani.dev@gmail.com>
2024-03-27 00:22:24 +0530

    advertise hero sec done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,11 @@
+// Navbar Toggle
 document
   .querySelector(".navbar-toggler")
   .addEventListener("click", function () {
     document.querySelector("html").classList.toggle("show-menu");
   });
+
+// Add class in Mega Menu
+document.querySelector("#mega-menu-btn").addEventListener("click", function () {
+  document.querySelector(".mega-menu-li").classList.toggle("show-mega-menu");
+});
